6cf95c2d483cf897e6ad60196f33d36c0671950e
Devon Lambert <devon@realityknights.com>
2024-02-21 17:43:36 -0600

    Basic Header and Footer

--- src/template/Footer.jsx
+++ src/template/Footer.jsx
@@ -1,12 +1,33 @@
-import { Box, Container } from '@chakra-ui/react'
-import { Placeholder } from '../components/Placeholder'
+import { ButtonGroup, Container, IconButton, Stack, Text } from '@chakra-ui/react'
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { Logo } from './Logo'
 
-export const Footer = (props) => {
-    return (
-        <Box as="footer" role="contentinfo" bg="bg.accent.default" {...props}>
-            <Container>
-                <Placeholder minH="20">Footer</Placeholder>
-            </Container>
-        </Box>
-    )
-}
+export const Footer = () => (
+  <Container
+    as="footer"
+    role="contentinfo"
+    py={{
+      base: '12',
+      md: '16',
+    }}
+  >
+    <Stack
+      spacing={{
+        base: '4',
+        md: '5',
+      }}
+    >
+      <Stack justify="space-between" direction="row" align="center">
+        <Logo />
+        <ButtonGroup variant="tertiary">
+          <IconButton as="a" href="#" aria-label="LinkedIn" icon={<FaLinkedin />} />
+          <IconButton as="a" href="#" aria-label="GitHub" icon={<FaGithub />} />
+          <IconButton as="a" href="#" aria-label="Twitter" icon={<FaTwitter />} />
+        </ButtonGroup>
+      </Stack>
+      <Text fontSize="sm" color="fg.subtle">
+        &copy; {new Date().getFullYear()} Chakra UI Pro, Inc. All rights reserved.
+      </Text>
+    </Stack>
+  </Container>
+)
--- src/template/Navbar.jsx
+++ src/template/Navbar.jsx
@@ -1,23 +1,48 @@
-import { Box, Container } from '@chakra-ui/react'
-import { Placeholder } from '../components/Placeholder'
-import { useNavbar } from '../hooks/useNavbar'
-
-export const Navbar = () => {
-    const { rootProps } = useNavbar()
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    Container,
+    HStack,
+    useBreakpointValue,
+    useDisclosure,
+  } from '@chakra-ui/react'
+  import { Logo } from './Logo'
+  import { MobileDrawer } from './MobileNavbar'
+  import { ToggleButton } from './ToggleButton'
+  
+  export const Navbar = () => {
+    const isDesktop = useBreakpointValue({
+      base: false,
+      lg: true,
+    })
+    const mobileNavbar = useDisclosure()
     return (
-        <Box
-            as="nav"
-            role="navigation"
-            position="sticky"
-            top="0"
-            zIndex="docked"
-            bg="bg.accent.default"
-            color="fg.accent.default"
-            {...rootProps}
-        >
-            <Container>
-                <Placeholder minH="20">Navigation</Placeholder>
-            </Container>
+      <Box borderBottomWidth="1px" bg="bg.surface" position="relative" zIndex="tooltip">
+          <Container py="4">
+            <HStack justify="space-between">
+              <Logo />
+              {isDesktop ? (
+                <HStack spacing="8">
+                  <ButtonGroup size="lg" variant="text" colorScheme="gray" spacing="8">
+                    {['Components', 'Pricing', 'Marketplace', 'Support'].map((item) => (
+                      <Button key={item}>{item}</Button>
+                    ))}
+                  </ButtonGroup>
+                  <Button>Sign Up</Button>
+                </HStack>
+              ) : (
+                <>
+                  <ToggleButton
+                    onClick={mobileNavbar.onToggle}
+                    isOpen={mobileNavbar.isOpen}
+                    aria-label="Open Menu"
+                  />
+                  <MobileDrawer isOpen={mobileNavbar.isOpen} onClose={mobileNavbar.onClose} />
+                </>
+              )}
+            </HStack>
+          </Container>
         </Box>
     )
-}
+  }
