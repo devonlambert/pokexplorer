19b00aa849158081f9c5fc21e047e791413b36d9
Devon Lambert <devon@realityknights.com>
2024-02-21 17:26:49 -0600

    Adds theme colors

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App.jsx'
 
+import '@fontsource-variable/open-sans'
+import '@fontsource-variable/spline-sans'
+
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { theme } from '@chakra-ui/pro-theme'
+import { extendTheme } from '@chakra-ui/react'
+
+const proTheme = extendTheme(theme)
+const extension = {
+  colors: { ...proTheme.colors, brand: proTheme.colors.blue },
+}
+const myTheme = extendTheme(extension, proTheme)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={myTheme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
